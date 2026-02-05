a4441281bfd9232185e270598152b12a2554f00a
Nicolina Wegert <nicolinawegert@gmail.com>
2026-02-05 14:42:40 +0100

    feat(book): set ISBN from constructor

--- LibrarySystem.Core/Models/Book.cs
+++ LibrarySystem.Core/Models/Book.cs
@@ -1,8 +1,13 @@
+using LibrarySystem.Core.Models;
+
 namespace LibrarySystem.Core.Models;
 
 public class Book
 {
+  public string ISBN { get; }
   public Book(string isbn, string title, string author, int publishedYear)
   {
+    ISBN = isbn;
+
   }
 }
