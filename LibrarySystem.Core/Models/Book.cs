feab68444970211ac4de6abd0e00cf71c27034f3
Nicolina Wegert <nicolinawegert@gmail.com>
2026-02-05 14:44:39 +0100

    feat(book): add PublishedYear property

--- LibrarySystem.Core/Models/Book.cs
+++ LibrarySystem.Core/Models/Book.cs
@@ -7,11 +7,12 @@ public class Book
   public string ISBN { get; }
   public string Title { get; }
   public string Author { get; }
+  public int PublishedYear { get; }
   public Book(string isbn, string title, string author, int publishedYear)
   {
     ISBN = isbn;
     Title = title;
     Author = author;
-
+    PublishedYear = publishedYear;
   }
 }
