216f54c6413a12ab914cd05025b529b978ce7a23
Nicolina Wegert <nicolinawegert@gmail.com>
2026-02-05 21:08:09 +0100

    refactor(catalog): encapsulate books collection

--- LibrarySystem.Core/Services/BookCatalog.cs
+++ LibrarySystem.Core/Services/BookCatalog.cs
@@ -1,15 +1,14 @@
-using LibrarySystem.Core;
 using LibrarySystem.Core.Models;
 
-namespace LibrarySystem.Core.Services
-{
-  public class BookCatalog
-  {
-    public List<Book> Books { get; } = new();
+namespace LibrarySystem.Core.Services;
 
-    public void Add(Book book)
-    {
-      Books.Add(book);
-    }
+public class BookCatalog
+{
+  private readonly List<Book> _books = new();
+  public IReadOnlyList<Book> Books => _books;
+
+  public void Add(Book book)
+  {
+    _books.Add(book);
   }
 }
