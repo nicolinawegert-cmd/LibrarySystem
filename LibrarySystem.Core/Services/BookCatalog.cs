17ab808d21c7c032349c2d01fb178e8327a5837d
Nicolina Wegert <nicolinawegert@gmail.com>
2026-02-05 20:56:38 +0100

    feat(catalog): add Books collection and Add method

--- LibrarySystem.Core/Services/BookCatalog.cs
+++ LibrarySystem.Core/Services/BookCatalog.cs
@@ -1,9 +1,15 @@
 using LibrarySystem.Core;
+using LibrarySystem.Core.Models;
 
 namespace LibrarySystem.Core.Services
 {
   public class BookCatalog
   {
+    public List<Book> Books { get; } = new();
 
+    public void Add(Book book)
+    {
+      Books.Add(book);
+    }
   }
 }
--- LibrarySystem.Core/Services/Library.cs
+++ LibrarySystem.Core/Services/Library.cs
@@ -1,4 +1,5 @@
 using LibrarySystem.Core.Services;
+using LibrarySystem.Core.Models;
 
 
 namespace LibrarySystem.Core.Services
