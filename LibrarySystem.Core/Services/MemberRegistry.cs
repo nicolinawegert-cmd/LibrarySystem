c9786a8c3cbaa7c90daaf21c1730942d9821e969
Nicolina Wegert <nicolinawegert@gmail.com>
2026-02-05 21:23:22 +0100

    feat(registry): add Members, Add and FindById

--- LibrarySystem.Core/Services/MemberRegistry.cs
+++ LibrarySystem.Core/Services/MemberRegistry.cs
@@ -5,5 +5,15 @@ namespace LibrarySystem.Core.Services;
 
 public class MemberRegistry
 {
-  
+  public List<Member> Members { get; } = new();
+
+  public void Add(Member member)
+  {
+    Members.Add(member);
+  }
+
+  public Member? FindById(string memberId)
+  {
+    return Members.FirstOrDefault(m => m.MemberId == memberId);
+  }
 }
