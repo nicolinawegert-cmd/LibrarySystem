dcbb1a700915aca51e2c2596d6287ba516bdb7f9
Nicolina Wegert <nicolinawegert@gmail.com>
2026-03-12 21:14:55 +0100

    feat(web): add Blazor Web App project and link Core/Data

---
+++ LibrarySystem.Web/Program.cs
@@ -0,0 +1,34 @@
+using LibrarySystem.Web.Components;
+using LibrarySystem.Data;
+using LibrarySystem.Data.Repositories;
+using LibrarySystem.Data.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseSqlite("Data Source=library.db"));
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<LoanService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
